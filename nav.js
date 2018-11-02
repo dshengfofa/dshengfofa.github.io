e8afe5139f185dc6b9dff37917deb7017281f757
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-11-02 16:31:04 +0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,1 @@
+﻿document.write("<ul><li><a href='index.html'>首页</li><li><a href='#'>视频</a></li><li><a href='#'>经阁</li><li><a href='./jiangzuo/jiangzuo.html'>讲座</li><li><a href='./jingji/index.html?f=8&v=20001'>语录</li><li><a href='./wenku/index.html?f=4&v=30001'>文库</li></ul>");
