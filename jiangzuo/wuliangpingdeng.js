e9e237b8ce067605cea392e58254a40558301447
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-11-14 13:54:40 +0800

    Add files via upload

---
+++ jiangzuo/wuliangpingdeng.js
@@ -0,0 +1,104 @@
+﻿if(GetQueryString('v')==606){
+document.write(
+
+"<li><a href='http://player.youku.com/embed/XMzUxNTUyNzU3Mg==' target='donghua'>第1集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzUyNzYyMzQ3Mg==' target='donghua'> 第2集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzUzOTM2NTg0MA==' target='donghua'>第3集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzU1MTE2Mjc3Ng==' target='donghua'>第4集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzU2MzExNzQ0NA==' target='donghua'>第5集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzU3NDYwMzQwMA==' target='donghua'>第6集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzU4NTI2NTkxMg==' target='donghua'>第7集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzYwMjk5NjMzMg==' target='donghua'>第8集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzYwNjYxOTQ2MA==' target='donghua'>第9集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzYxNjUxOTUyOA==' target='donghua'>第10集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzYyNTI5MDA4OA==' target='donghua'>第11集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzYzNDM1MTc2NA==' target='donghua'>第12集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY0NDEwNDAxNg==' target='donghua'>第13集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY1NDIyMDk0NA==' target='donghua'>第14集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY2MzQ0OTg2MA==' target='donghua'>第15集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY3MzA1MTE4NA==' target='donghua'>第16集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY4MzAzNDIwNA==' target='donghua'>第17集刘素云老师试讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzY5MzMxODE0NA==' target='donghua'>第18集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzcwMjg3MTkwOA==' target='donghua'>第19集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzcxMjU0NjMzMg==' target='donghua'>第20集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzcyMjM5NzU5Mg==' target='donghua'>第21集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzczMTc1NzgyNA==' target='donghua'>第22集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc0MjExOTE4NA==' target='donghua'>第23集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc1MTk4MDQ4MA==' target='donghua'>第24集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc2MTEwNTM2NA==' target='donghua'>第25集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc2OTA1ODA4OA==' target='donghua'>第26集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc3Nzg5NjQ4NA==' target='donghua'>第27集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc4NjUwNjgwNA==' target='donghua'>第28集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzc5MzY2MTM4NA==' target='donghua'>第29集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgwMDYwMTg1Mg==' target='donghua'>第30集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgwNzU1Mzk1Mg==' target='donghua'>第31集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgxMzk3NjEwNA==' target='donghua'>《佛学大乘无量寿庄严清净平等觉经》第32集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgyMDM2MzI3Mg==' target='donghua'>第33集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgyNjU4ODYyNA==' target='donghua'>第34集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgzMDQ3ODQ1Mg==' target='donghua'>第35集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgzNDQxMzA4MA==' target='donghua'>第36集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzgzNzg5MTg2NA==' target='donghua'>第37集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg0MjIyMzgwNA==' target='donghua'>第38集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg0NTUxNTA4NA==' target='donghua'>第39集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg0NzYyMjgyOA==' target='donghua'>第40集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg1MDAzMTkyMA==' target='donghua'>第41集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg1MjI0Mzg5Mg==' target='donghua'>第42集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg1NDc3MTE4NA==' target='donghua'>第43集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg1Nzg2MjgwMA==' target='donghua'>第44集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg2MDY0Njg4MA==' target='donghua'>第45集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg2MzM5MzQzNg==' target='donghua'>第46集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg2ODMxNTY1Ng==' target='donghua'>第47集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg3MjY0MDc5Ng==' target='donghua'>第48集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg3NjU1NDY3Mg==' target='donghua'>第49集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg3ODIzMTk4OA==' target='donghua'>第50集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg3OTM4MDQzNg==' target='donghua'>第51集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg3OTgyMDk2OA==' target='donghua'>第52集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4MTU4NzA5Mg==' target='donghua'>第53集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4MTYwNTY3Ng==' target='donghua'>第54集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4Mjc0MjE3Ng==' target='donghua'>第55集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4Mjc3Mzk2NA==' target='donghua'>第56集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4NDY5NDY0MA==' target='donghua'>第57集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4NDc0NjEwMA==' target='donghua'>第58集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4NjQ1MTc5Ng==' target='donghua'>第59集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4NjQ2MDk0OA==' target='donghua'>第60集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4ODAyNDg0OA==' target='donghua'>第61集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4ODA1NDYyNA==' target='donghua'>第62集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4OTg5Mjc4OA==' target='donghua'>第63集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg4OTkzNzc2NA==' target='donghua'>第64集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5MTQ5NTkxMg==' target='donghua'>第65集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5MTUwMTUxMg==' target='donghua'>第66集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5MzA5NzMyMA==' target='donghua'>第67集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5MzEwMTAxNg==' target='donghua'>第68集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5NDgyMjY0NA==' target='donghua'>第69集_刘素云老师复讲</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzg5NDgyNzU0MA==' target='donghua'>第70集_刘素云老师复讲</a></li>");
+}
+
+if(GetQueryString('v')==609){
+document.write(
+
+"<li><a href='http://player.youku.com/embed/XMzQ2MDk4OTM5Mg==' target='donghua'>24</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDkzNjM0OA==' target='donghua'>23</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDg4NDg0MA==' target='donghua'>22</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDc4OTUwOA==' target='donghua'>21</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDcyNzI0MA==' target='donghua'>20</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDY1MDQ1Mg==' target='donghua'>19</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDU3NTE3Ng==' target='donghua'>18</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDUwNTIwOA==' target='donghua'>17</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDQ0ODk3Mg==' target='donghua'>16</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDM5MDM4OA==' target='donghua'>15</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDMzMjQ0OA==' target='donghua'>14</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDI0MTk1Mg==' target='donghua'>13</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDE3ODI4OA==' target='donghua'>12</a></li>","<li><a href='http://player.youku.com/embed/XMzQ2MDA1MjY3Mg==' target='donghua'>11</a></li>","<li><a href='http://player.youku.com/embed/XMzQ1OTk5OTk4MA==' target='donghua'>10</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MDEyMDQwMA==' target='donghua'>9</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTk0NTQ0OA==' target='donghua'>8</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTg4MDMyNA==' target='donghua'>7</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTgzMDEyMA==' target='donghua'>6</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTc1NzU4OA==' target='donghua'>5</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTY5NjEyMA==' target='donghua'>4</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTYzNzk0NA==' target='donghua'>3</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTU4Nzc3Ng==' target='donghua'>2</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ1OTU0MzQ4NA==' target='donghua'>1</a></li>",
+"<li><a href='http://player.youku.com/embed/XMzQ2MTE0MTE0OA==' target='donghua'>三皈依传授</a></li>"
+);}
+
+
